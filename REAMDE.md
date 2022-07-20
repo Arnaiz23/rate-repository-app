839e29fbff73fe7b601d4207ccb9fa8a61537871
Arnaiz23 <aaf110501@gmail.com>
2022-07-20 15:33:50 +0200

    feat: v1.0.0 	* Create the repository and init

---
+++ REAMDE.md
@@ -0,0 +1,3 @@
+# Rate Repository App
+
+Repository of the project from the course of [React Native MiduDev](https://www.youtube.com/watch?v=qi87b6VcIHY&t=4761s)
---
+++ src/components/RepositoryItem.js
@@ -0,0 +1,57 @@
+import { View, StyleSheet, Image } from "react-native"
+import StyledText from "./StyleText"
+import theme from "../theme"
+import RepositoryStats from "./RepositoryStats.js"
+
+const RepositoryItemHeader = ({
+  ownerAvatarUrl,
+  fullName,
+  description,
+  language,
+}) => (
+  <View style={{ flexDirection: "row", paddingBottom: 2 }}>
+    <View style={{ paddingRight: 10 }}>
+      <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+    </View>
+    <View style={{ flex: 1 }}>
+      <StyledText fontWeight="bold">{fullName}</StyledText>
+      <StyledText color="secondary">{description}</StyledText>
+      <StyledText style={styles.language}>{language}</StyledText>
+    </View>
+  </View>
+)
+
+const RepositoryItem = (props) => (
+  <View key={props.id} style={styles.container}>
+    <RepositoryItemHeader {...props} />
+    <RepositoryStats {...props} />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  id: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  language: {
+    padding: 4,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.primary,
+    alignSelf: "flex-start",
+    marginVertical: 4,
+    borderRadius: 4,
+    overflow: "hidden",
+  },
+  image: {
+    width: 48,
+    height: 48,
+    borderRadius: 4,
+  },
+})
+
+export default RepositoryItem
