b98b252e98ffbe859a78d2ee5c07d3ff14702825
Arnaiz23 <aaf110501@gmail.com>
2022-07-21 14:20:05 +0200

    feat: v1.0.1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,24 @@
-import { View } from "react-native"
-import Constants from "expo-constants"
-import RepositoryList from "./RepositoryList"
+import { Text, View } from 'react-native'
+import RepositoryList from './RepositoryList.jsx'
+import AppBar from './AppBar.js'
+import { Routes ,Route } from 'react-router-native'
 
 const Main = () => {
   return (
-    <View
-      style={{
-        marginTop: Constants.statusBarHeight,
-        flexGrow: 1,
-      }}
-    >
-      <RepositoryList />
+    <View style={{ flex: 1 }}>
+      {/* <AppBar /> */}
+      <hola bb>
+      <Routes>
+        <Route path='/' exact element={<RepositoryList />} />
+        <Route path='/signin' exact element={<Signin />} />
+        <Route path='/other' exact element={<Text>Other page</Text>} />
+      </Routes>
     </View>
   )
+}
+
+const Signin = () => {
+  return <Text>Working on it</Text>
 }
 
 export default Main
